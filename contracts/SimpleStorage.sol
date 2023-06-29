f6017f058e7c9bb7b6be7d58ee826be5ed805217
Dahir Muhammad Dahir <dahirmuhammad3@gmail.com>
2023-06-29 05:47:10 +0100

    basic data types

---
+++ contracts/SimpleStorage.sol
@@ -0,0 +1,16 @@
+// SPDX-License-Identifier: MIT
+// -=-<[ Bismillahirrahmanirrahim ]>-=-
+// -*- coding: utf-8 -*-
+// @Date    : 2023-06-28 14:07:19
+// @Author  : Dahir Muhammad Dahir (dahirmuhammad3@gmail.com)
+pragma solidity 0.8.18;
+
+contract SimpleStorage {
+    bool hasFavoriteNumber = true;
+    uint256 favoriteNumber = 88;
+    string favoriteNumberInText = "eighty-eight";
+    int256 favoriteInt = -88;
+    address myAddress = 0xaF3070c5B6b0206f5fB8c2f8C43173d1779F030F;
+    bytes32 favoriteBytes32 = "cat";
+}
+
