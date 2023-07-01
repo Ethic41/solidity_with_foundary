aaa1a278088426de7824ee20e2757ea6079a95ea
Dahir Muhammad Dahir <dahirmuhammad3@gmail.com>
2023-07-01 18:08:23 +0100

    implemented withdraw

--- contracts/FundMe.sol
+++ contracts/FundMe.sol
@@ -14,11 +14,35 @@ contract FundMe {
     uint256 public minimumUSD = 5e18;
     address[] public funders;
     mapping(address funder => uint256 amountFunded) public addressToAmountFunded;
+
+    address public owner;
+
+    constructor() {
+        owner = msg.sender;
+    }
     
     function fund() public payable {
         require(msg.value.getCoversionRate() >= minimumUSD, "not enough ETH");
         funders.push(msg.sender);
-        addressToAmountFunded[msg.sender] = addressToAmountFunded[msg.sender] + msg.value;
+        addressToAmountFunded[msg.sender] += msg.value;
+    }
+
+    function withdraw() public onlyOwner {
+
+        for (uint256 funderIndex = 0; funderIndex < funders.length; funderIndex++){
+            address funder = funders[funderIndex];
+            addressToAmountFunded[funder] = 0;
+        }
+
+        funders = new address[](0);
+
+        (bool callSuccess, ) = payable(msg.sender).call{value: address(this).balance}("");
+        require(callSuccess, "Call failed");
+    }
+
+    modifier onlyOwner() {
+        require(msg.sender == owner, "Sender is not owner!");
+        _;
     }
 
 }
