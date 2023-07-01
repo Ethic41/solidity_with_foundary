d1b561ab21f6a64f882c861877bc104055690213
Dahir Muhammad Dahir <dahirmuhammad3@gmail.com>
2023-07-01 11:01:27 +0100

    storage factory

---
+++ contracts/StorageFactory.sol
@@ -0,0 +1,31 @@
+// SPDX-License-Identifier: MIT
+// -=-<[ Bismillahirrahmanirrahim ]>-=-
+// -*- coding: utf-8 -*-
+// @Date    : 2023-06-29 07:42:48
+// @Author  : Dahir Muhammad Dahir (dahirmuhammad3@gmail.com)
+pragma solidity ^0.8.18;
+
+import {SimpleStorage} from "./SimpleStorage.sol";
+
+
+contract StorageFactory {
+    
+    SimpleStorage[] public listOfSimpleStorageContracts;
+
+    function createSimpleStorageContract() public {
+        SimpleStorage newSimpleStorageContract = new SimpleStorage();
+        listOfSimpleStorageContracts.push(newSimpleStorageContract);
+    }
+
+    function sfStore(uint256 _simpleStorageIndex, uint256 _newSimpleStorageNumber) public {
+        SimpleStorage mySimpleStorage = listOfSimpleStorageContracts[_simpleStorageIndex];
+        mySimpleStorage.store(_newSimpleStorageNumber);
+    }
+
+    function sfGet(uint256 _simpleStorageIndex) public view returns(uint256) {
+        return listOfSimpleStorageContracts[_simpleStorageIndex].retrieve();        
+    }
+
+}
+
+
